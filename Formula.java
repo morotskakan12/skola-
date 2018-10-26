c71cd8508adad14712598a5b99c799f36019bb94
morotskakan12 <Ballder1>
2018-10-26 15:06:49 +0200

    kalar med kelvin och vattentryk. samt komentera kod

--- Formula.java
+++ Formula.java
@@ -1,12 +1,26 @@
 
 	public class Formula {
 		
-		public static double fahrenheitToCelsius(double fahrenheit);
+		public static double fahrenheitToCelsius(double fahrenheit){
 		
 		double Celsius = (fahrenheit-32)*5/9;
 		
-		return celsius;
-		
+		return Celsius;
+		}
+		public static double kelvinToCelsius(double kelvin){
+			
+		double Celsius = (kelvin-273.15);
+			return Celsius;
+		}
+		public static double fluidPressure(LibraryLiquids fluid, double deep){
+			double p = 0; // Pascals
+			double d = fluid.density; // densitet
+			double h = deep; //hjupet
+			double g = 9.82; // gravitation
+			p = (d*h*g);
+			
+			return p;	
+		}
 		
 	}
 	
