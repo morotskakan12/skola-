08f72a3b05b6f10ce73ac18aa7498bc765c9b1c2
morotskakan12 <Ballder1>
2018-11-09 15:32:59 +0100

    klar med gund uppgift.

--- VisaBild.java
+++ VisaBild.java
@@ -2,38 +2,31 @@
 	import java.awt.event.*;
 	import javax.swing.*;
 	public class VisaBild extends JFrame implements ActionListener {
-		private JButton b1 = new JButton ("Liquids List");
-		private JButton b2 = new JButton ("Soled List");
-		private	JButton b3 = new JButton ("Gas List");
-		private	JButton b4 = new JButton ("Formula");
-		private JButton b5 = new JButton ("exit");
+		private JButton b1 = new JButton (" List");
+		private	JButton b2 = new JButton ("Formula");
+		private JButton b3 = new JButton ("exit");
 		private JPanel p5 = new JPanel();
 
 		public VisaBild(){
 			p5.setBackground(Color.white);
 			p5.setPreferredSize(new Dimension(70,20));
 			setLayout(new FlowLayout());
-			add(b1);add(b2);add(b3);add(b4);add(b5);add(p5);
+			add(b1);add(b2);add(b3);add(p5);
 			b1.setBorderPainted(false);b1.addActionListener(this);
 			b2.setBorderPainted(false);b2.addActionListener(this);
 			b3.setBorderPainted(false);b3.addActionListener(this);
-			b4.setBorderPainted(false);b4.addActionListener(this);
-			b5.setBorderPainted(false);b5.addActionListener(this);
 			setSize(350,250);
 			setVisible(true);
 			setDefaultCloseOperation(EXIT_ON_CLOSE);
 		}		
 		public void actionPreformed(ActionEvent e){
+			tabel.tabel tabel =new tabel.tabel(); 
 			if (e.getSource() == b1);
-				
+				tabel();
 			else if(e.getSource() == b2);
 				
 			else if(e.getSource() == b3);
-			
-			else if(e.getSource() == b4);
-			
-			else if(e.getSource() == b5)
-			;
+
 		}
 		
 		
--- tabel.java
+++ tabel.java
@@ -1,31 +1,31 @@
-	import java.arw.*;
+	import java.awt.*;
 	import javax.swing.*;
-	public class tabel extends JFframe {
+	public class tabel extends JFrame {
 		private JButton b1 =new JButton("1");
 		private JButton b2=new JButton("2");
 		private JButton b3=new JButton("3");
 		private JButton b4=new JButton();
 		private JLabel L5=new JLabel("hej");
-		private JTextArea = new JTextArea("en arbetsyta");
+		private JTextArea a = new JTextArea("en arbetsyta");
 		
 		public tabel (){
 			JTabbedPane tp = new JTabbedPane();
 			add(tp);
 			
 			JPanel kort1 = new JPanel();
-			tp.addTab("spel",kort1);
+			tp.addTab("Gas",kort1);
 			kort1.setLayout(new GridLayout(2,2));
 			kort1.add(b1);kort1.add(b2);kort1.add(b3);kort1.add(b4);
 			
-			Jpanel kort2 = new JPanel();
-			tp.addTab ("arbete", kort2);
+			JScrollPane kort2 = new JScrollPane();
+			tp.addTab ("Solid", kort2);
 			
-			JPanel kort3 = new JPanel();
-			tp.addTab("blomma"null, kort3,);
+			JScrollPane kort3 = new JScrollPane();
+			tp.addTab("Liquids", kort3);
 			
 			setSize(250,250);
 			setVisible(true);
-				setDefaultColseOperation(EXIT_ON_CLOSE)
+				setDefaultCloseOperation(EXIT_ON_CLOSE);
 			
 		}
 
