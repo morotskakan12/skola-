9b866bc52ad721ae9e0e4293c80a1076a01f13eb
morotskakan12 <Ballder1>
2018-12-07 17:27:24 +0100

    Update Nummber.java
    jag känner att jag är klar.

--- arryar/src/arryar/Nummber.java
+++ arryar/src/arryar/Nummber.java
@@ -11,7 +11,7 @@ public class Nummber {
 	static String[] names = {"Crystal","Tam","Ed","Beulah","Daina","Benjamin","Kia","Clelia","Cassy","Gita","Celsa","Karoline","Talitha","Lewis","Betsy","Colin","Glendora","Carola","Rosalba","Jeanie","Yevette","Armand","Neal","Lilla","Dorethea","Delta","Maye","Nikita","Shoshana","Carola","Margie","Haywood","Venessa","Natacha","Gilbert","Kandi","Tyisha","Tammie","Blossom","Penney","Diana","Audrey","Willard","Zoraida","Drusilla","Jacquline","Cyndy","Janiece","Tressie","Kami","Lashanda","Leann","Tom","Santana","Junita","Gisela","Tom","Marquerite","Bryant","Lauralee","Yael","Kelle","Samantha","Tom","Meta","Lanette","Wanetta","Carola","Jana","Neal","Brady","Rigoberto","Felicia","Hellen","Georgeann","Carola","Isaias","Ellis","Roseanne","Lenard","Ela","Ophelia","Alesha","Mafalda","Flor","Kelsi","Autumn","Sondra","Pasty","Jacquelyne","Benjamin","Emmie","Mickie","Lang","Jamee","Felice","Daniella","Carola","Nathalie","Genevive"};
 	 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// kör alla mitoder 1-10
 		System.out.println(amunt());
 		System.out.println(tom());
 		System.out.println(itNumber());
@@ -21,12 +21,15 @@ public class Nummber {
 		System.out.println(letterFive());
 		howMenny();
 		System.out.println(unik());
+		System.out.println(alleNams());
 	}
 	// 100 tal i intervallet [0-9]
+	//mitod nr1
 	public static int amunt () {
-		 int tims = numbers.length;
-		
+		//akpar aktuela variabler. 
+		int tims = numbers.length;
 			int ansor = 0;
+			//identiifera alla numer av värder 7.
 			for(int i = 0 ; i < tims; i++ ) {
 				
 				if (numbers[i] == 7) {
@@ -35,10 +38,12 @@ public class Nummber {
 			}
 			return ansor;
 		 }
-
+//metod 2
 	public static int tom () {
+		//skapa aktuella variabeler.
 		int ansor = 0;
 		int tims = names.length;
+		//identiferar altuela namn i listan och läger till ett vid träff
 		for(int i = 0 ; i < tims; i++ ) {
 			if (names[i].equals("Tom")||names[i].equals("tom")) {
 				ansor++;
@@ -47,14 +52,14 @@ public class Nummber {
 	}
 			return ansor;
 	}
-
+// metod 3
 	public static int itNumber() {
 		// skapar variabler 
+		int  outnum2 = 0;
 		int ansor =0;
 		int tims = numbers.length;
 		int conter = 0;
-		int getMinValue = numbers.length;
-		boolean what = false;
+		
 		// skaper arrey
 		int [] index = new int [10];
 		// tästa nummer mellan 1-9
@@ -70,28 +75,23 @@ public class Nummber {
 				}
 			}
 		// hitta det minsta värdet
-		for(int i=0;i<10;i++ ) {
-			if (numbers[i]< getMinValue&&numbers[i]!=0) {
-			
-				getMinValue = numbers[i];
-			}
-		}
-		int i = 0;
-			while(what== false) { 
-				
-					
-				if (numbers[i]==getMinValue) {
-				ansor = i;
-				what = true;			
-				}
-			i = i+1;
-			}
+		        for (int i = 0; i < index.length - 1; i++) {
+		          
+		            if ( outnum2 > index[i+1]) {
+		              outnum2 = i+1;
+		            }
+		        }
+	  //retunera svar
+		        ansor = numbers[outnum2];
 		return ansor;
 		
 		}
+	//metod4
 	public static int Drusilla() {
+		//skapa aktuella variabler.
 		int ansor = 0;
 		int tims = names.length;
+		//söker aktuelt namn och retunerarn platsen i listan 
 		for(int i = 0 ; i < tims; i++ ) {
 			if (names[i].equals("Drusilla")){
 				ansor= i;
@@ -99,9 +99,12 @@ public class Nummber {
 		}
 		return ansor;
 	}
+	//metod 5
 	public static int equol() {
+		//skaper aktuela variaber
 		int ansor = 0;
 		int tims = numbers.length;
+		//söker efeer och adderar aktuella värden 
 		for(int i = 0 ; i < tims; i++ ) {
 			if(numbers[i]==2||numbers[i]==4||numbers[i]==6||numbers[i]==8) {
 				ansor= ansor+ numbers[i];
@@ -111,10 +114,13 @@ public class Nummber {
 		
 		return ansor;
 	}
+//metod6	
 	public static int letterL() {
+		//skaper aktuella variabler
 		int ansor = 0;
 		int tims = names.length;
 		char oneLetter;
+		//söker för en angiven bokstav på fursta platsen i stringen. ifall träff addera med ett i int
 		for(int i = 0 ; i < tims; i++ ) { 
 			oneLetter = names[i].charAt(0);
 			
@@ -125,10 +131,13 @@ public class Nummber {
 		}
 		return ansor;
 	}
+//mwetod7	
 	public static int letterFive() {
-		 String str;
+		//skapa aktuella variabeler  
+		String str;
 		 int ansor =0;
 		 int tims = names.length;
+		 //kollar hela listan eferer or som ineholler fem bokstäver;
 		for(int i = 0 ; i < tims; i++ ) { 
 			str = names[i];
 			
@@ -138,46 +147,95 @@ public class Nummber {
 		}
 		return ansor;
 	}
+//method 8
 	public static void howMenny () {
+		//skaper aktuella variabeler + lista med 10 platser 
 		int tims = numbers.length;
 		int conter = 0;
 		int [] indexx = new int [10];
+		//lägger till väden i listat indexx
 		for(int i = 0 ; i < 10; i++ ) {
 			conter = 0;
+			//söker efeter dubbleter av en sifra och räknar dem.
 			for(int e = 1; e<tims; e++) {
 				if (numbers[e] == i) {
 					conter++;
 				}
+				//lägger till resultatet från söknigen 
 				indexx[i]=conter;
 			}
 		}
+		//skriver ut listan med tillhörande sifra. 
 		for (int i = 0; i<10; i++) {
 			System.out.println(indexx[i]+"of nr"+i);
 		}
 	}
+//metod 9 
 	public static int unik() {
+		//skapar aktuella variabler 
 		int ansor = 0 ;
 		int tims = names.length;
 		String bas;
+		// analyserar om det fin dubletter 
 		for(int i = 0 ; i < tims; i++ ) {
 			bas = names[i];
 			for (int e = 0; e<tims; e++) {
+				//stop för att inte gämföra med sin egen plats.
 				if (i!=e) {
+					//bryt lopp och tästa nästa om den hittar dubletter anars lägg till värde 
 					if(bas.equals(names[e])) {
 						break;	
 					}
 					if(e==tims-1) {
 						ansor++;
 				}	
-					}
+			}
 
 				
 			}
 		}
 		return ansor;
 		}
-		
-		
-	}
-
-
+//mehod 10
+	 public static String alleNams() {    //10
+		//skapar aktuella variabler 
+		 String  ansor = "" ;
+			int tims = names.length;
+			String bas;
+			int outnum2 = 0;
+			int apa = 0 ;
+			// skapat lista med 100 platser. 
+			//med tanke att varge int plats i listan ska ha en motsvarighet i namnlistan.
+			
+			int [] how = new int [100];
+			// avgör hur många namn det fin av ett namn och gar en motsvarande sifra i how
+			for(int i = 0 ; i < tims; i++ ) {
+				bas = names[i];
+				//avgör och lägger till motsvarande sifra.
+				for (int e = 0; e<tims; e++) {
+					
+						if(bas.equals(names[e])) {
+						apa++;
+						}
+				}
+				how[i] = apa;
+				}
+				// ser vilket som är det sstörsta värdet i listan. 
+				// jag inser att det kommer att finas flera nomnet med det största antalet.
+				//så den kommer att velja den sitat i raden om kommer att var nomnet på motsvarande plat i namn listan. 
+				//
+				for (int z = 0; z < how.length; z++) {
+					for(int e =0; e<how.length;e++) {
+		            if ( how[z] > how[e]) {
+		            	outnum2 = z;
+					            }
+					}
+					        }
+					
+	        
+	    
+			
+			ansor=names[outnum2];
+			return ansor;
+	 }
+}
